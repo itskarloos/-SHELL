0448e07a0b5af5bc263bee61e0528800f51afb7e
Michael <mikenegh@gmail.com>
2025-02-14 12:33:10 +0300

    simple shell

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    input()
+    command = input()
+    if command:
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
