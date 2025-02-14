308659ddf4428482b81d00e1f412f6d377c38547
Michael <mikenegh@gmail.com>
2025-02-14 13:37:13 +0300

    sixth  challenge

--- app/main.py
+++ app/main.py
@@ -12,7 +12,11 @@ def main():
             result = " ".join(output)
             print(f"{result}")
             continue
-
+        if commandFrag[0] == "type":
+            if commandFrag[1] == "echo" | commandFrag[1] == "exit":
+                print(f"{commandFrag[1]} is a shell builtin ")
+            else:
+                print(f"{commandFrag[1]}: not found")
         if command == "exit 0":
             return
         if command:
