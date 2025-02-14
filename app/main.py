6e810eb57523eccf4292fade64a569ac66d64243
Michael <mikenegh@gmail.com>
2025-02-14 12:59:37 +0300

    third challenge

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
+from telnetlib import DO
 import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    if command:
-        print(f"{command}: command not found")
+    while (True):
+        sys.stdout.write("$ ")
+        command = input()
+        if command:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
