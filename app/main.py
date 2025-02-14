119374f5049ead355fd226492553ef324ae3cae1
Michael <mikenegh@gmail.com>
2025-02-14 13:07:54 +0300

    fourth challenge

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     while (True):
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit 0":
+            return
         if command:
             print(f"{command}: command not found")
 
