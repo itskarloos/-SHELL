ee9d647719972af56f37366ed320a0775fb8d444
Michael <mikenegh@gmail.com>
2025-02-14 21:11:19 +0300

    seventh  challenge

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def main():
         path_file["ls"] = "/usr/bin/ls"
         path_file["cp"] = "/bin/cp"
         path_file["mkdir"] = "/bin/mkdir"
-        path_file["my_exe"] = "/tmp/quz/my_exe"
+        path_file["my_exe"] = "/tmp/baz/my_exe"
 
     while (True):
         sys.stdout.write("$ ")
