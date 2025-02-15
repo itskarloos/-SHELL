e681632b25b62087cff7c159b790b950e73b9097
Michael Seyoum <michaelseyoum@Michaels-iMac.local>
2025-02-15 15:57:18 +0300

    [any message]
    merge conflict solved

--- app/main.py
+++ app/main.py
@@ -5,6 +5,22 @@ import subprocess
 
 
 def main():
+
+    sys_path = os.environ["PATH"].split(os.pathsep)
+
+    path_file = {
+        "cat": "/bin/cat",
+        "ls": "/usr/bin/ls",
+        "cp": "/bin/cp",
+        "mkdir": "/bin/mkdir",
+        "my_exe": "/tmp/quz/my_exe",
+    }
+
+    for paths in sys_path:
+        parts = paths.split("/")
+        if len(parts) > 1:
+            path_file[parts[1]] = paths
+
     while True:
         sys.stdout.write("$ ")
         command = input().strip()
@@ -12,6 +28,7 @@ def main():
             continue
 
         commandFrag = command.split(" ")
+
         program = commandFrag[0]
 
         # Check if it's a built-in command
@@ -20,10 +37,27 @@ def main():
                 print(" ".join(commandFrag[1:]))
 
             case "type":
-                if commandFrag[1] in ["echo", "exit", "type","pwd"]:
+                if commandFrag[1] in ["echo", "exit", "type", "pwd"]:
                     print(f"{commandFrag[1]} is a shell builtin")
                 elif shutil.which(commandFrag[1]):  # Check if it's in PATH
-                    print(f"{commandFrag[1]} is {shutil.which(commandFrag[1])}")
+                    print(f"{commandFrag[1]} is {
+                          shutil.which(commandFrag[1])}")
+
+        if os.path.isfile(commandFrag[0]):  # Fix condition outside match
+            os.system(command)
+            continue
+
+        match commandFrag[0]:
+            case "echo":
+                output = commandFrag[1:]
+                print(" ".join(output))
+
+            case "type":
+                if commandFrag[1] in ["echo", "exit", "type"]:
+                    print(f"{commandFrag[1]} is a shell builtin")
+                elif commandFrag[1] in path_file:
+                    print(f"{commandFrag[1]} is {path_file[commandFrag[1]]}")
+
                 else:
                     print(f"{commandFrag[1]}: not found")
 
@@ -32,16 +66,21 @@ def main():
                 sys.exit(exit_code)
 
             case _:
-                executable_path = shutil.which(program)
-                if executable_path:  # If found in PATH, execute it
-                    try:
-                        subprocess.run(commandFrag)  # Run with arguments
-                    except Exception as e:
-                        print(f"Error executing {program}: {e}")
-                else:
-                    print(f"{program}: command not found")
+
+
+<< << << < HEAD
+  executable_path = shutil.which(program)
+   if executable_path:  # If found in PATH, execute it
+        try:
+            subprocess.run(commandFrag)  # Run with arguments
+        except Exception as e:
+            print(f"Error executing {program}: {e}")
+    else:
+        print(f"{program}: command not found")
+== == ===
+  print(f"{command}: command not found")
+>>>>>> > 90370ee([any message])
 
 
 if __name__ == "__main__":
     main()
-
